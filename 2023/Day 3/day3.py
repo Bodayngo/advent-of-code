387ae4d5aadf059d1113a7d9844ce182fbef8723
Evan Wilkerson <bodayngo@gmail.com>
2023-12-03 08:58:37 -0600

    removed comments/unused debugging print statements

--- 2023/Day 3/day3.py
+++ 2023/Day 3/day3.py
@@ -9,7 +9,6 @@ def part1():
     with open(input_file, 'r') as f:
         all_number_matches = []
         input_lines = f.read().splitlines()
-        # Get numbers
         for line in input_lines:
             line_number_matches = []
             nunmber_matches = re.finditer(r"[0-9]+", line)
@@ -17,17 +16,13 @@ def part1():
                 number_match = (match.group(), match.start(), match.end())
                 line_number_matches.append(number_match)
             all_number_matches.append(line_number_matches)
-        # Check if numbers are adjacent to symbols
         for line, number_matches in enumerate(all_number_matches):
             for number_match in number_matches:
                 valid = False
-                #print(str(line) + ': ' + str(number_match))
                 for l in range(-1, 2):
                     if line+l >= 0 and line+l < len(input_lines):
-                        #print(input_lines[line+l])
                         symbol_results = re.finditer(r"[^\d.]", input_lines[line+l])
                         for symbol in symbol_results:
-                            #print(symbol)
                             if symbol.start() >= number_match[1]-1 and symbol.start() <= number_match[2]:
                                 valid = True
                 if valid == True:
@@ -39,23 +34,19 @@ def part2():
     with open(input_file, 'r') as f:
         all_symbol_matches = []
         input_lines = f.read().splitlines()
-        # Get numbers
         for line in input_lines:
             line_symbol_matches = []
             symbol_matches = re.finditer(r"[*]", line)
             for match in symbol_matches:
                 line_symbol_matches.append(match.start())
             all_symbol_matches.append(line_symbol_matches)
-        # Check if numbers are adjacent to symbols
         for line, symbol_matches in enumerate(all_symbol_matches):
             for symbol_match in symbol_matches:
                 adj_numbers = []
-                #print(str(line) + ': ' + str(symbol_match))
                 for l in range(-1, 2):
                     if line+l >= 0 and line+l < len(input_lines):
                         number_matches = re.finditer(r"[0-9]+", input_lines[line+l])
                         for number_match in number_matches:
-                            #print(number_match)
                             if symbol_match >= number_match.start()-1 and symbol_match <= number_match.end():
                                 adj_numbers.append(number_match.group())
                 if len(adj_numbers) == 2:
