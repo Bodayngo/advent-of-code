11d7a329d951189a4128ce650634d8d6dbde5835
Evan Wilkerson <bodayngo@gmail.com>
2023-12-03 08:56:27 -0600

    part 2

--- 2023/Day 3/day3.py
+++ 2023/Day 3/day3.py
@@ -31,13 +31,43 @@ def part1():
                             if symbol.start() >= number_match[1]-1 and symbol.start() <= number_match[2]:
                                 valid = True
                 if valid == True:
-                    valid_number_sum+=int(number_match[0])
+                    valid_number_sum += int(number_match[0])
         return(valid_number_sum)
-
+    
+def part2():
+    valid_number_sum = 0
+    with open(input_file, 'r') as f:
+        all_symbol_matches = []
+        input_lines = f.read().splitlines()
+        # Get numbers
+        for line in input_lines:
+            line_symbol_matches = []
+            symbol_matches = re.finditer(r"[*]", line)
+            for match in symbol_matches:
+                line_symbol_matches.append(match.start())
+            all_symbol_matches.append(line_symbol_matches)
+        # Check if numbers are adjacent to symbols
+        for line, symbol_matches in enumerate(all_symbol_matches):
+            for symbol_match in symbol_matches:
+                adj_numbers = []
+                #print(str(line) + ': ' + str(symbol_match))
+                for l in range(-1, 2):
+                    if line+l >= 0 and line+l < len(input_lines):
+                        number_matches = re.finditer(r"[0-9]+", input_lines[line+l])
+                        for number_match in number_matches:
+                            #print(number_match)
+                            if symbol_match >= number_match.start()-1 and symbol_match <= number_match.end():
+                                adj_numbers.append(number_match.group())
+                if len(adj_numbers) == 2:
+                    ratio = int(adj_numbers[0]) * int(adj_numbers[1])
+                    valid_number_sum += ratio
+        return valid_number_sum
 
 def main():
     part1_result = part1()
     print(f"Part 1 answer: {part1_result}")
+    part2_result = part2()
+    print(f"Part 2 answer: {part2_result}")
 
 if __name__ == '__main__':
     main()
