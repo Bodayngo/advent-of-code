7845867e4da112e9fd708bdf0138980fbc892493
Evan Wilkerson <evan.wilkerson@meraki.net>
2023-12-05 10:06:53 -0600

    added comment

--- 2023/Day 4/day4.py
+++ 2023/Day 4/day4.py
@@ -19,6 +19,8 @@ def process_line(line):
                 points = 1
             else:
                 points *= 2
+
+    # Return the scratched and winning number lists
     return winning_numbers, scratched_numbers
 
 # Function for Part 1
