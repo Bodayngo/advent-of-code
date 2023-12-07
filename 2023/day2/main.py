eb80c311122a2b522d8b97e15f3a7d473de419e7
Evan Wilkerson <bodayngo@gmail.com>
2023-12-07 06:42:39 -0600

    test

---
+++ 2023/day2/main.py
@@ -0,0 +1,42 @@
+import os
+import re
+
+def parse_line(line):
+    split_line = re.split(': |; |, ', line)
+    game_number = int(split_line.pop(0).split(' ')[1])
+    dice_counts_tuples = [(int(item.split()[0]), item.split()[1]) for item in split_line]
+    dice_counts_tuples.sort(reverse=True)
+    return game_number, dice_counts_tuples
+
+def part1(input_lines):
+    limits = {'red': 12, 'green': 13, 'blue': 14}
+    passed_games = [
+        game_number
+        for game_number, counts in (parse_line(line) for line in input_lines)
+        if all(
+            count[0] <= limits[count[1]]
+            for count in counts
+        )
+    ]
+    return sum(passed_games)
+
+def part2(input_lines):
+    powers = []
+    for _, counts in (parse_line(line) for line in input_lines):
+        highest_red = next((t for t in counts if 'red' in t), None)[0]
+        highest_green = next((t for t in counts if 'green' in t), None)[0]
+        highest_blue = next((t for t in counts if 'blue' in t), None)[0]
+        power = highest_red * highest_green * highest_blue
+        powers.append(int(power))
+    return sum(powers)
+
+def main():
+    directory = os.path.dirname(os.path.realpath(__file__))
+    input_file = os.path.join(directory, 'puzzle_input.txt')
+    with open(input_file, 'r') as file:
+        input_lines = file.read().strip().splitlines()
+    print(f"Part 1 answer: {part1(input_lines)}")
+    print(f"Part 2 answer: {part2(input_lines)}")
+
+if __name__ == '__main__':
+    main()
