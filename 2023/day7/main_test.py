8ddc11b18dba14332266c68aeb4ece1736d42731
Evan Wilkerson <bodayngo@gmail.com>
2023-12-08 07:43:13 -0600

    y tho

--- 2023/day7/main_test.py
+++ 2023/day7/main_test.py
@@ -1,37 +1,33 @@
 import os
 from collections import Counter
 
+""""
+3
+('33663', '564')
+('2Q922', '859')
+('28522', '965')
+('22TT2', '72')
+('22992', '108')
+('22552', '201')
+4
+('AAKKK', '886')
+('AA8A3', '504')
+('AA4AJ', '345')"""
 
 def get_hand_ranking(hand):
-    """
-    1: Five of a kind
-    2: Four of a kind
-    3: Full house
-    4: Three of a kind
-    5: Two pair
-    6: One pair
-    7: High card
-    """
     card_counts = Counter(hand)
     max_occurrence = max(card_counts.values())
-    #ranking_dict = {5: 1, 4: 2, 3: 3 if hand[0] == hand[-1] and len(set(hand[1:4])) == 1 else 4, 2: 5 if list(card_counts.values()).count(2) == 2 else 6}
+    #ranking_dict = {5: 1, 4: 2, 3: 3 if hand[0] == hand[-1] else 4, 2: 5 if list(card_counts.values()).count(max_occurrence) == 2 else 6}
     #return ranking_dict.get(max_occurrence, 7)
     if max_occurrence == 5:
-        return 1 # Five of a kind
+        return 1 # Five of kind
     elif max_occurrence == 4:
         return 2 # Four of a kind
     elif max_occurrence == 3:
-        if hand[0] == hand[-1] and len(set(hand[1:4])) == 1:
+        if hand[0] == hand[-1]:
             return 3 # Full house
         else:
             return 4 # Three of a kind
-    elif max_occurrence == 2:
-        if list(card_counts.values()).count(2) == 2:
-            return 5 # Two pair
-        else:
-            return 6 # One pair
-    else:
-        return 7 # High card
 
 def custom_order(character):
     order = {'A': 1, 'K': 2, 'Q': 3, 'J': 4, 'T': 5, '9': 6, '8': 7, '7': 8, '6': 9, '5': 10, '4': 11, '3': 12, '2': 13}
@@ -48,11 +44,13 @@ def part1(input_lines):
     for line in input_lines:
         hand, bid = line.split(' ')[:2]
         hand_rankings_by_type[get_hand_ranking(hand)].append((hand, bid))
-    for hand_type in hand_rankings_by_type.keys():
-        hand_rankings_by_type[hand_type] = sorted(hand_rankings_by_type[hand_type], key=custom_sort)
-        print(hand_type)
-        for hand in hand_rankings_by_type[hand_type]:
-            print(hand)
+    for type_ranking in hand_rankings_by_type.keys():
+        hand_rankings_by_type[type_ranking] = sorted(hand_rankings_by_type[type_ranking], key=custom_sort)
+
+        print(type_ranking)
+        for value in hand_rankings_by_type[type_ranking]:
+            print(value)
+
     sorted_bid_list = [
         value[1] 
         for values in hand_rankings_by_type.values() 
@@ -69,7 +67,7 @@ def part2(input_lines):
 
 def main():
     directory = os.path.dirname(os.path.realpath(__file__))
-    input_file = f"{directory}/puzzle_input.txt"
+    input_file = f"{directory}/new_input.txt"
     with open(input_file, 'r') as file:
         input_lines = file.read().strip().splitlines()
     print(f"Part 1 answer: {part1(input_lines)}")
